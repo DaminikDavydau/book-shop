b773f6763daecd5965f387e70a66a21a84b80885
DaminikDavydau <113989900+DaminikDavydau@users.noreply.github.com>
2022-11-22 21:49:35 +0200

    fix: link

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 function version() {
             let version = new DocumentFragment();
             let tag = document.createElement("h1")
-            tag.textContent = "v.1.5.7"
+            tag.textContent = "v.1.5.8"
             version.append(tag);
             document.querySelector("body").append(version);
 }
@@ -84,7 +84,7 @@ fetch('./books.json') //path to the file with json data
 
 let bag = document.getElementsById("bag");
 bag.setAttribute('ondrop', "drop(event)")
-bag.setAttribute('ondragover', "allowDrop(event)"
+bag.setAttribute('ondragover', "allowDrop(event)")
 
 
 function popup(i) {
